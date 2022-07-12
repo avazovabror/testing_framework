4e0fd9e375e84ff4c192fa010840e4d4d910c601
Abror2199 <avazov_abror21@mail.ru>
2022-07-12 15:09:08 +0500

    initialization webdriver

--- src/test/java/webtesting/AmazonTest.java
+++ src/test/java/webtesting/AmazonTest.java
@@ -1,10 +1,12 @@
 package webtesting;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class AmazonTest {
     public static void main(String[] args) {
-        System.setProperty("webdriver.chrome.driver", "");
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/driver/chromedriver");
+        WebDriver driver = new ChromeDriver();
     }
 
 }
