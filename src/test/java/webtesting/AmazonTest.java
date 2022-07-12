8562d40436a84e1c1ccb1a653c8ca12584c5c790
Abror2199 <avazov_abror21@mail.ru>
2022-07-12 15:03:09 +0500

    First commit

---
+++ src/test/java/webtesting/AmazonTest.java
@@ -0,0 +1,10 @@
+package webtesting;
+
+import org.openqa.selenium.WebDriver;
+
+public class AmazonTest {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.chrome.driver", "");
+    }
+
+}
